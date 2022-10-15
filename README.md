a1bb6501ae33d5028bc6d6c705acd803140eaabf
baba-s <shotababa1206@gmail.com>
2022-10-15 09:23:27 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.AutoEnableComponent
+# Kogane Auto Enable Component
+
+シーン開始時にコンポーネントを自動で有効にするコンポーネント
---
+++ Runtime/AutoEnableComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kogane.Internal
+{
+    [DefaultExecutionOrder( -1000 )]
+    [DisallowMultipleComponent]
+    internal sealed class AutoEnableComponent : MonoBehaviour
+    {
+        [SerializeField]                  private MonoBehaviour m_monoBehaviour;
+        [SerializeField][HideInInspector] private bool          m_isExecuted;
+
+        private void Awake()
+        {
+            if ( m_isExecuted ) return;
+            m_monoBehaviour.enabled = true;
+            m_isExecuted            = true;
+        }
+    }
+}
